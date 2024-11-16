23ed538ca23499d3b5586fdc53cc8bc7bb0cece5
Sergey Malyshev <mal_sn@mail.ru>
2024-11-16 11:28:02 +0300

    Добавлено изменение размера изображения под размер окна; file_menu; в меню добавлено открытие изображения в новом окне;

--- Cataas.py
+++ Cataas.py
@@ -10,6 +10,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
@@ -23,16 +24,40 @@ def set_image():
         label.image = img
 
 
+def open_new_window():
+    img = load_image(url)
+
+    if img:
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
+        label.image = img
+
+
+def exit():
+    window.destroy()
+
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
 label = Label()
 label.pack()
 
 update_button = Button(text="Обновить", command=set_image)
 update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото в новом окне", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
 
